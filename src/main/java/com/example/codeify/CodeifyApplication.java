b3abe997c99a56b646e5056077c5f63b5c329e92
smath <smathewslynch@gmail.com>
2025-01-27 12:41:45 +0000

    Initial Commit

---
+++ src/main/java/com/example/codeify/CodeifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codeify/CodeifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

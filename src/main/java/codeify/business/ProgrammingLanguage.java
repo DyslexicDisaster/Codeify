cd7958041e2d0b62b8030c5af88626be1624d732
Malikom12 <muhammadmalikomer@gmail.com>
2025-02-01 19:24:24 +0000

    Updated questions business model, created and implemented questions dao, dto controller

---
+++ src/main/java/codeify/business/ProgrammingLanguage.java
@@ -0,0 +1,13 @@
+package codeify.business;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgrammingLanguage {
+    private int id;
+    private String name;
+}

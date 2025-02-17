86608396cd9b212f393661a9edf3625b8bb0e261
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-02-17 16:05:14 +0000

    Created repositories for implemented methods that will be called inside controllers.

---
+++ src/main/java/codeify/repository/QuestionRepository.java
@@ -0,0 +1,13 @@
+package codeify.repository;
+
+import codeify.model.ProgrammingLanguage;
+import codeify.model.Question;
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public interface QuestionRepository {
+    List<Question> getQuestionByLanguage(int languageId) throws SQLException;
+}

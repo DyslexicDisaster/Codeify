3cf7758cd74946f8f4a26e55843435509d6ef624
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-02-18 12:00:04 +0000

    Created class that will configure API key authentication in the application.

--- src/main/java/codeify/controllers/ProgrammingLanguageController.java
+++ src/main/java/codeify/controllers/ProgrammingLanguageController.java
@@ -4,9 +4,7 @@ import codeify.model.ProgrammingLanguage;
 import codeify.persistance.ProgrammingLanguageRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;

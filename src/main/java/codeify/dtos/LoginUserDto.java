69a04b4746188da2adb9786cf3da026aa25de7c1
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-10 17:51:36 +0000

    Dto's are holding values during registration and login. LoginResponse is used to return JWT token and expiration period to the user after login.

---
+++ src/main/java/codeify/dtos/LoginUserDto.java
@@ -0,0 +1,13 @@
+package codeify.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUserDto {
+    private String username;
+    private String password;
+}

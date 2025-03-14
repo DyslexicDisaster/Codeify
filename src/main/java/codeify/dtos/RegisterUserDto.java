a2411192d38193958f7fc689c27ea1daaa2404ba
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-03-14 01:07:53 +0000

    Minor update.

--- src/main/java/codeify/dtos/RegisterUserDto.java
+++ src/main/java/codeify/dtos/RegisterUserDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterUserDto {
-    private String email;
     private String username;
     private String password;
+    private String email;
 }
